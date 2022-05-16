ffdefa95e4c2affe9df2c84ec24dbdceba958760
VinuRamanan <vinufutureastronaut@gmail.com>
2022-05-16 13:15:35 +0530

    string - palindrome

---
+++ prep-lib/src/main/java/com/interview/prep/palindrome/QPalindrome.java
@@ -0,0 +1,24 @@
+package main.java.com.interview.prep.palindrome;
+
+/*
+
+Given a string, check if the string is a palindrome or not by implementing the isPalindrome function
+Eg: abcvcba = true, abcvabc = false
+
+ */
+public class QPalindrome {
+
+    public static boolean isPalindromeApproach1(String s) {
+        // Implement the function
+        return true;
+    }
+
+    public static boolean isPalindromeApproach2(String s) {
+        // Implement the function
+        return true;
+    }
+
+    public static void main(String args[]) {
+        // TODO: For testing
+    }
+}
---
+++ prep-lib/src/main/java/com/interview/prep/palindrome/QPlaindromeStringCharacterCount.java
@@ -0,0 +1,36 @@
+package main.java.com.interview.prep.palindrome;
+
+/*
+
+Given a string, find the number of character changes, to make the string a palindrome.
+
+Eg :
+abcvxbz
+We can change a - z, c - x
+Ans: 3
+
+
+abccba
+No changes required
+Ans: 0
+
+
+
+ */
+
+public class QPlaindromeStringCharacterCount {
+
+    public static int getCharacterCountApproach1(String s) {
+        // Implement the function
+        return 0;
+    }
+
+    public static int getCharacterCountApproach2(String s) {
+        // Implement the function
+        return 0;
+    }
+
+    public static void main(String args[]) {
+        // TODO: For testing
+    }
+}
